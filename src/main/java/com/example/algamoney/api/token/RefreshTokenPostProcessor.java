895e3933fc0d761166c1306bb58dab8278c58f27
Evandro Silva <evandro.silva@grupoavenida.com.br>
2024-11-18 23:36:51 -0300

    Implementação token com refresh token e filtro para CORS

--- src/main/java/com/example/algamoney/api/token/RefreshTokenPostProcessor.java
+++ src/main/java/com/example/algamoney/api/token/RefreshTokenPostProcessor.java
@@ -1,9 +1,5 @@
 package com.example.algamoney.api.token;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -15,6 +11,10 @@ import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("deprecation")
 @ControllerAdvice
